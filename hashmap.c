3f377d8b7432fa5fb897fc801c98e24f0838da97
BenjaminSepulvedaO <163463275+BenjaminSepulvedaO@users.noreply.github.com>
2024-04-25 21:49:44 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -84,7 +84,7 @@ void eraseMap(HashMap * map,  char * key) {
     {
       if (is_equal(map->buckets[pos]->key, key))
       {
-        map->buckets[pos] = NULL;
+        map->buckets[pos]->key = NULL;
         map->size--;
         return;
       }
