8fa6ae06dc9512feee9f7996a1b9981171c50141
BenjaminSepulvedaO <163463275+BenjaminSepulvedaO@users.noreply.github.com>
2024-04-25 21:55:00 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -118,7 +118,7 @@ Pair * firstMap(HashMap * map) {
   long i;
   for (i = 0; i < map->capacity; i++)
     {
-      if (map->buckets[i] != NULL)
+      if (map->buckets[i]->key != NULL)
       {
         map->current = i;
         return map->buckets[i];
@@ -130,7 +130,7 @@ Pair * firstMap(HashMap * map) {
 Pair * nextMap(HashMap * map) {
   for (long i = map->current + 1; i < map->capacity; i++)
     {
-      if (map->buckets[i] != NULL)
+      if (map->buckets[i]->key != NULL)
       {
         map->current = i;
         return map->buckets[i];
