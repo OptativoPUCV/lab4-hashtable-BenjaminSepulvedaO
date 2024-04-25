1678dc9044fc918d898699d329af0e054fd49ff3
BenjaminSepulvedaO <163463275+BenjaminSepulvedaO@users.noreply.github.com>
2024-04-25 21:54:26 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -115,11 +115,26 @@ Pair * searchMap(HashMap * map,  char * key) {
 }
 
 Pair * firstMap(HashMap * map) {
-
-    return NULL;
+  long i;
+  for (i = 0; i < map->capacity; i++)
+    {
+      if (map->buckets[i] != NULL)
+      {
+        map->current = i;
+        return map->buckets[i];
+      }
+    }
+  return NULL;
 }
 
 Pair * nextMap(HashMap * map) {
-
-    return NULL;
+  for (long i = map->current + 1; i < map->capacity; i++)
+    {
+      if (map->buckets[i] != NULL)
+      {
+        map->current = i;
+        return map->buckets[i];
+      }
+    }
+  return NULL;
 }
